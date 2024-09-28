df9cf8568778947bc563d94b6a0d416ce28d5932
Daniel Getachew <dani.gechoo@gmail.com>
2024-09-29 00:58:57 +0300

    shopping_list_manager module added

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,35 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            shopping_list.append(input("Enter the item: "))
+        elif choice == '2':
+            # Prompt for and remove an item
+            item = input("Enter the item to remove: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} is removed!")
+            else :
+                print(f"{item} is not in your shopping list\n")
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
