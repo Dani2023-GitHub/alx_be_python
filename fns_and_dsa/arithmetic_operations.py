a05b90e1484535d0c229e9f6ef4663bde10d8ac4
Daniel Getachew <dani.gechoo@gmail.com>
2024-09-29 00:24:33 +0300

    arithmetic_operations module added

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Division by zero is invalid"
-            else:
+            elif num2 != 0:
                 return num1 / num2
         case _:
             return "Invalid operation!"
