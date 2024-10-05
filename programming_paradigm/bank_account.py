5a8f5c65bf6a4a2a483338503d19148ca0fe6da6
Daniel Getachew <dani.gechoo@gmail.com>
2024-10-05 23:46:05 +0300

    Bank_account module added

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return 1
+        else: 
+            return 0
+    def display_balance(self):
+        print(f"Current balance: {self.account_balance}")
