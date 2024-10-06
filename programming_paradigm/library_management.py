acaf3d22d972b90ac77250a835d155703266d1e5
Daniel Getachew <dani.gechoo@gmail.com>
2024-10-06 14:09:23 +0300

    Library_management.py module added

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -18,22 +18,22 @@ class Book:
 
 class Library:
     def __init__(self):
-        self.__books = []
+        self._books = []
 
     def add_book(self, book):
-        self.__books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title:
                 book.check_out_book()
     
     def return_book(self,title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title:
                 book.return_book()
 
     def list_available_books(self):
-        for book in self.__books:
+        for book in self._books:
             if book.get_status() == False:
                 print(book.title, book.author)       
