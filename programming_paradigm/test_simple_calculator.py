1925df077919c3333549b0d490e9ebb87f94a441
Daniel Getachew <dani.gechoo@gmail.com>
2024-10-06 11:17:36 +0300

    test_simple_calculator.py module added

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,20 +12,20 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
-    def test_subract(self):
+    def test_subraction(self):
         """Test the subtraction method"""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(1, 10), -9)
         self.assertEqual(self.calc.subtract(10, -3), 13)
         self.assertEqual(self.calc.subtract(-1, 3), -4)
         
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 2), 4)
         self.assertEqual(self.calc.multiply(-5, 2), -10)
         self.assertEqual(self.calc.multiply(-2, -5), 10)
         self.assertEqual(self.calc.multiply(2, 0), 0)
     
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(0, 2), 0)
         self.assertEqual(self.calc.divide(10, -20), -0.5)
